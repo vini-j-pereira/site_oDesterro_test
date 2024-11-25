4005c664746639515e37f81d2c87c6559e7b3f3b
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-11-24 23:04:37 -0300

    feat: js menu resp

---
+++ JS/scripts.js
@@ -0,0 +1,9 @@
+let navbarList = document.getElementById('navbar-list');
+let navbarIcons = document.getElementById('navbar-icons');
+let spanAnimado = document.getElementById('span-animado');
+
+function menuResp(){
+    navbarList.classList.toggle('navbar-list--show-links');
+    navbarIcons.classList.toggle('navbar-icons--show-links');
+    spanAnimado.classList.toggle('span-animado--show-links')
+}
